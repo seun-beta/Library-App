d7d7624862ba69cf44a7761576fe19ae1320927a
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-04-25 21:01:50 +0100

    create model for library books

--- library/models.py
+++ library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Lang(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    isbn = models.CharField(max_length=13)
+    language = models.ForeignKey('Lang', on_delete=models.SET_NULL, null=True)
+
+
+class Instance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    due_back = models.DateField(null=True, blank=True)
+    
